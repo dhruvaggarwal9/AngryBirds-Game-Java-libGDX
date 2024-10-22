c3d9da2aa4cba9958d748cb9b05e6c5e802317e2
Dhruv Aggarwal <dhruv23197@iiitd.ac.in>
2024-10-22 08:28:52 +0530

    Update MainScreen.java

--- Game/core/src/main/java/com/angryBird/Screens/MainScreen.java
+++ Game/core/src/main/java/com/angryBird/Screens/MainScreen.java
@@ -2,7 +2,6 @@ package com.angryBird.Screens;
 
 import com.angryBird.Main;
 import com.angryBird.objects.Button;
-import com.angryBird.objects.Level;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -10,59 +9,103 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.angryBird.objects.Pig;
 
 public class MainScreen implements Screen {
     private SpriteBatch spriteBatch;
     private Texture backgroundTexture;
     private Sprite backgroundSprite;
-    private Button startButton;
+    private Button singlePlayerButton;
+    private Button multiPlayerButton;
+    private Button profileButton;
     private Main game;
+
+
+    private static final float Y_OPTIONS = 31f;
+
+    private int hoveredButtonIndex = -1;
+
+    private static final float BUTTON_GAP = 50f; // Gap between buttons
+    private static final float hovervalue = 1.1f; // Hover scale factor
 
     public MainScreen(Main game) {
         this.game = game;
         spriteBatch = new SpriteBatch();
 
-
         backgroundTexture = new Texture("MainScreen.png");
         backgroundSprite = new Sprite(backgroundTexture);
         backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        startButton = new Button("start.png");
+        singlePlayerButton = new Button("mainplay.png");
+        multiPlayerButton = new Button("multiplayer.png");
+        profileButton = new Button("profile.png");
 
-        Sprite btnSprite = startButton.getButtonSprite();
-        btnSprite.setSize(500, 300);
-        btnSprite.setPosition(
-                (Gdx.graphics.getWidth() - btnSprite.getWidth()) / 2,
-                 Gdx.graphics.getHeight() / 9
-        );
+        float buttonWidth = 400f;
+        float buttonHeight = 200f;
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        Sprite singlePlayerSprite = singlePlayerButton.getButtonSprite();
+        Sprite multiPlayerSprite = multiPlayerButton.getButtonSprite();
+        Sprite profileSprite = profileButton.getButtonSprite();
+
+        singlePlayerSprite.setSize(buttonWidth, buttonHeight);
+        multiPlayerSprite.setSize(buttonWidth, buttonHeight);
+        profileSprite.setSize(buttonWidth, buttonHeight);
+
+        singlePlayerSprite.setPosition(BUTTON_GAP, Y_OPTIONS);
+        multiPlayerSprite.setPosition(singlePlayerSprite.getX() + singlePlayerSprite.getWidth() + BUTTON_GAP, Y_OPTIONS);
+        profileSprite.setPosition(multiPlayerSprite.getX() + multiPlayerSprite.getWidth() + BUTTON_GAP, Y_OPTIONS);
     }
 
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(Color.BLACK);
+        ScreenUtils.clear(0, 0, 0, 0);
 
         handleInput();
 
         spriteBatch.begin();
         backgroundSprite.draw(spriteBatch);
-        startButton.getButtonSprite().draw(spriteBatch);
+
+        drawButton(singlePlayerButton.getButtonSprite(), 0);
+        drawButton(multiPlayerButton.getButtonSprite(), 1);
+        drawButton(profileButton.getButtonSprite(), 2);
+
         spriteBatch.end();
     }
 
+
+    private void drawButton(Sprite buttonSprite, int buttonIndex) {
+        if (hoveredButtonIndex == buttonIndex) {
+            buttonSprite.setScale(hovervalue);
+        } else {
+            buttonSprite.setScale(1.0f);
+        }
+        buttonSprite.draw(spriteBatch);
+    }
+
+
     private void handleInput() {
+        float touchX = Gdx.input.getX();
+        float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+        hoveredButtonIndex = -1;
+
+        if (singlePlayerButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+            hoveredButtonIndex = 0;
+        } else if (multiPlayerButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+            hoveredButtonIndex = 1;
+        } else if (profileButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+            hoveredButtonIndex = 2;
+        }
+
         if (Gdx.input.justTouched()) {
-
-            float touchX = Gdx.input.getX();
-            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
-
-            if (startButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
-
-                Pig pig = new Pig("bigPig", "p1.png", 100, 100);
-                Button pauseButton = new Button("pause.png");
-                Level level = new Level("scene2.png", pig, pauseButton);
-                game.setScreen(new LevelScreen(level, game));
+            if (hoveredButtonIndex == 0) {
+                game.setScreen(new SeasonsListScreen(game));
+            } else if (hoveredButtonIndex == 1) {
+                //multiplayer button click
+            } else if (hoveredButtonIndex == 2) {
+                //profile button click
             }
         }
     }
@@ -73,11 +116,24 @@ public class MainScreen implements Screen {
         backgroundSprite.setSize(width, height);
 
 
-        Sprite btnSprite = startButton.getButtonSprite();
-        btnSprite.setPosition(
-                (width - btnSprite.getWidth()) / 2,
-                height / 9
-        );
+        float buttonWidth = width * 0.3f;
+        float buttonHeight = height * 0.175f;
+
+        Sprite singlePlayerSprite = singlePlayerButton.getButtonSprite();
+        Sprite multiPlayerSprite = multiPlayerButton.getButtonSprite();
+        Sprite profileSprite = profileButton.getButtonSprite();
+
+        singlePlayerSprite.setSize(buttonWidth, buttonHeight);
+        multiPlayerSprite.setSize(buttonWidth, buttonHeight);
+        profileSprite.setSize(buttonWidth, buttonHeight);
+
+
+        singlePlayerSprite.setPosition(BUTTON_GAP, Y_OPTIONS);
+        multiPlayerSprite.setPosition(singlePlayerSprite.getX() + singlePlayerSprite.getWidth() + BUTTON_GAP, Y_OPTIONS);
+        profileSprite.setPosition(multiPlayerSprite.getX() + multiPlayerSprite.getWidth() + BUTTON_GAP, Y_OPTIONS);
+
+
+        spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
     }
 
 
