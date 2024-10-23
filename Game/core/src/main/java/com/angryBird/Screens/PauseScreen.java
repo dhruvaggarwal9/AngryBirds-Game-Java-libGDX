da29b989f0173fc1ad4cbecaf29e051e5fa38023
pranavkadam27 <162269173+pranavkadam27@users.noreply.github.com>
2024-10-23 22:05:05 +0530

    update PauseScreen.java

--- Game/core/src/main/java/com/angryBird/Screens/PauseScreen.java
+++ Game/core/src/main/java/com/angryBird/Screens/PauseScreen.java
@@ -1,22 +1,86 @@
 package com.angryBird.Screens;
 
 import com.angryBird.Main;
+import com.angryBird.objects.Button;
+import com.angryBird.objects.Level;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class PauseScreen implements Screen {
     
+    // Texture background;
+    Button resume;
+    Button returnHome;
     Main game;
-    public PauseScreen(Main game){
+    LevelScreen levelScreen;
 
+    Sprite backSprite;
+    Sprite resumeSprite;
+    Sprite returnSprite;
+    SpriteBatch spriteBatch;
+    Vector2 touch;
+    public PauseScreen(Main game,LevelScreen level){
+
+        
+        resume = new Button("resume.png");
+        returnHome = new Button("returnHome.png");
+        this.game = game;
+        this.levelScreen = level;
+
+        backSprite = new Sprite(new Texture("season1Back.png"));
+        resumeSprite = resume.getButtonSprite();
+        returnSprite = returnHome.getButtonSprite();
+        spriteBatch = new SpriteBatch();
+        touch = new Vector2();
+
+        setPostions();
+    }
+
+    public void setPostions(){
+
+
+        float width = Gdx.graphics.getWidth()/10;
+        float height = Gdx.graphics.getHeight()/10;
+
+        backSprite.setSize(10*width, 10*height);
+        resumeSprite.setBounds(4*width,5*height,7*(resumeSprite.getWidth()), 3*(resumeSprite.getHeight()));
+        returnSprite.setBounds(4*width,5*height-resumeSprite.getHeight(),7*(returnSprite.getWidth()), 3*(returnSprite.getHeight()));
     }
 
 
+    public void handleInput(){
 
+        if (Gdx.input.justTouched()) {
 
+            touch.set(Gdx.input.getX(),Gdx.graphics.getHeight()-Gdx.input.getY());
+
+        
+            if(returnSprite.getBoundingRectangle().contains(touch)){
+                levelScreen.dispose();
+                game.setScreen(new MainScreen(game));
+            }
+            else if(resumeSprite.getBoundingRectangle().contains(touch)){
+                game.setScreen(levelScreen);
+            }
+          
+        }
+    }
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+
+        handleInput();
+        spriteBatch.begin();
+
+        backSprite.draw(spriteBatch);
+        resumeSprite.draw(spriteBatch);
+        returnSprite.draw(spriteBatch);
+
+        spriteBatch.end();
     }
         
     @Override
