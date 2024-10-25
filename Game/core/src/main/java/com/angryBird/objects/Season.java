08498d58e2d299bf3cf27d64fd6522bcc5d1d707
pranavkadam27 <162269173+pranavkadam27@users.noreply.github.com>
2024-10-25 15:33:40 +0530

    updating Season.java

--- Game/core/src/main/java/com/angryBird/objects/Season.java
+++ Game/core/src/main/java/com/angryBird/objects/Season.java
@@ -9,13 +9,15 @@ public class Season {
     String seasonName;
     Texture seasonBackground;
     Texture seasonTheme;
+    Texture blurBackgroundImage;
     ArrayList<Level> levels;
     //SEASON SCREEN WILL HAVE RECTANGLES CREATED TO SHOW LEVELS
-    public Season(String seasonName,String backgroundImg,String themeImg,ArrayList<Level> levels){
+    public Season(String seasonName,String backgroundImg,String themeImg,String blurBackgroundImage,ArrayList<Level> levels){
 
         this.seasonName = seasonName;
         this.seasonBackground = new Texture(backgroundImg);
         this.seasonTheme = new Texture(themeImg);
+        this.blurBackgroundImage = new Texture(blurBackgroundImage);
         this.levels = levels;
     }
 
@@ -27,6 +29,9 @@ public class Season {
         return seasonBackground;
     }
 
+    public Texture getBlurBackground(){
+        return blurBackgroundImage;
+    }
     public Texture getThemeImg(){
         return seasonTheme;
     }
