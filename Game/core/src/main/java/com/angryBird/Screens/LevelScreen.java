2ce22f1905a88fb99380015e9c74947423027bd2
Dhruv Aggarwal <dhruv23197@iiitd.ac.in>
2024-11-29 19:37:48 +0530

    Update LevelScreen.java

--- Game/core/src/main/java/com/angryBird/Screens/LevelScreen.java
+++ Game/core/src/main/java/com/angryBird/Screens/LevelScreen.java
@@ -12,12 +12,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -43,6 +53,20 @@ public class LevelScreen implements Screen {
     private SpriteBatch spriteBatch;
 
 
+
+
+    World world;
+    OrthographicCamera camera;
+    Box2DDebugRenderer debugRenderer;
+    Body platform;
+    private ArrayList<Body> birdBodies;
+    private ArrayList<Body> pigBodies;
+    private ArrayList<Body> blockBodies;
+
+
+    static final float lamda = 10f;  //(1 meter = 10 pixels)
+
+
     public LevelScreen(Main game,Level level){
 
         birdBase = new Texture("base.png");
@@ -58,22 +82,89 @@ public class LevelScreen implements Screen {
         birdSprites = new ArrayList<Sprite>();
         pigSprites = new ArrayList<Sprite>();
         backgroundSprite = level.getBackSprite();
-        blockSprites = level.getBuilding().getBlockSprites();
+        // blockSprites = level.getBuilding().getBlockSprites();
         groundLayer = new Sprite(new Texture("groundlayer2.png"));
         touch = new Vector2();
         spriteBatch = new SpriteBatch();
 
-        for (Bird bird : game.getBirdsUnlocked()) {
-            birdSprites.add(new Sprite(bird.getImg()));
-        }
+        // for (Bird bird : game.getBirdsUnlocked()) {
+        //     birdSprites.add(new Sprite(bird.getImg()));
+        // }
 
-        for (Pig pig : level.getPigEnemies()) {
-            pigSprites.add(new Sprite(pig.getImg()));
-        }
+        // for (Pig pig : level.getPigEnemies()) {
+        //     pigSprites.add(new Sprite(pig.getImg()));
+        // }
 
         setPosition();
+        create();
     }
 
+    public Vector2 screenPosition(Vector2 pos){
+		Vector2 offset = new Vector2(camera.viewportWidth/2, camera.viewportHeight/2);
+		pos = pos.add(offset);
+		return pos.scl(lamda);
+	}
+
+    public Vector2 screenPosition(float x,float y){
+
+        Vector2 pos = new Vector2(x,y);
+		Vector2 offset = new Vector2(camera.viewportWidth/2, camera.viewportHeight/2);
+		pos = pos.add(offset);
+		return pos.scl(lamda);
+	}
+    public Vector2 worldPosition(float x,float y){
+
+        Vector2 offset = new Vector2(camera.viewportWidth/2, camera.viewportHeight/2);
+        Vector2 pos = new Vector2(x,y);
+        pos = pos.scl(lamda);
+        pos = pos.sub(offset);
+
+		return pos;
+    }
+    public void create(){
+
+        world = new World(new Vector2(0, -10), true);
+        camera = new OrthographicCamera(192, 108);				//whole screen is (192 meters, 108 meters);
+        debugRenderer = new Box2DDebugRenderer();
+        birdBodies = new ArrayList<Body>();
+        BodyDef staticBody = new BodyDef();
+		staticBody.type = BodyType.StaticBody;
+
+        platform = world.createBody(staticBody);
+        EdgeShape edge1 = new EdgeShape();
+        edge1.set(new Vector2(-camera.viewportWidth-2, -camera.viewportHeight/2+10), new Vector2(camera.viewportWidth, -camera.viewportHeight/2+10));
+
+        FixtureDef platformProperties = new FixtureDef();
+		platformProperties.shape = edge1;
+		platformProperties.density = 0.5f;
+		platformProperties.friction = 0.4f;
+		platformProperties.restitution = 0.6f;
+        Fixture platformFixture = platform.createFixture(platformProperties);
+
+
+        BodyDef dynamicBody = new BodyDef();
+		dynamicBody.type = BodyType.DynamicBody;
+		// dynamicBody.position.set(-camera.viewportWidth/2+30, -camera.viewportHeight/2+10);
+        CircleShape circle1 = new CircleShape();
+        circle1.setRadius(10f);
+        FixtureDef birdProperties = new FixtureDef();
+		birdProperties.shape = circle1;
+		birdProperties.density = 0.5f;
+		birdProperties.friction = 0.5f;
+		birdProperties.restitution = 0.6f;
+
+
+
+        for (Sprite bird : birdSprites) {
+            Vector2 pos = worldPosition(bird.getX(), bird.getY());
+            dynamicBody.position.set(pos.x,pos.y);
+            birdBodies.add(world.createBody(dynamicBody));
+            birdBodies.getLast().setUserData(bird);
+            Fixture birdFixture = birdBodies.getLast().createFixture(birdProperties);
+        }
+
+
+    }
     public void setPosition() {
         // Resize your screen here. The parameters represent the new window size.
         float width = Gdx.graphics.getWidth()/10;
@@ -107,13 +198,13 @@ public class LevelScreen implements Screen {
         }
 
         //setPostion// of pig
-        level.getBuilding().translate(pigBaseSprite.getX(), pigBaseSprite.getY() + pigBaseSprite.getHeight()+10); // translating sprites stored in building object realtive to level positions
+        // level.getBuilding().translate(pigBaseSprite.getX(), pigBaseSprite.getY() + pigBaseSprite.getHeight()+10); // translating sprites stored in building object realtive to level positions
 
-        ArrayList<Rectangle> emptySpaces = level.getBuilding().getEmptySpaces();
+        // ArrayList<Rectangle> emptySpaces = level.getBuilding().getEmptySpaces();
         for(int i = 0; i< pigSprites.size(); i++){
             pigSprites.get(i).setSize(50, 50);
             Vector2 center = new Vector2();
-            emptySpaces.get(i).getCenter(center);
+            // emptySpaces.get(i).getCenter(center);
             pigSprites.get(i).setPosition(center.x-10, center.y-30);
             // pigSprites.get(i).getBoundingRectangle().fitInside(emptySpaces.get(i));
         }
@@ -129,15 +220,15 @@ public class LevelScreen implements Screen {
 
     public void handleInput(){
 
-        if (Gdx.input.justTouched()) {
-
-            touch.set(Gdx.input.getX(),Gdx.graphics.getHeight()-Gdx.input.getY());
-
-            if(pauseButtonSprite.getBoundingRectangle().contains(touch)){
-                game.setScreen(new PauseScreen(game,this));
-            }
-
-        }
+//        if (Gdx.input.justTouched()) {
+//
+//            touch.set(Gdx.input.getX(),Gdx.graphics.getHeight()-Gdx.input.getY());
+//
+//            if(pauseButtonSprite.getBoundingRectangle().contains(touch)){
+//                game.setScreen(new PauseScreen(game,this));
+//            }
+//
+//        }
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
             dispose();
             game.setScreen(new WinScreen(game));
@@ -173,11 +264,18 @@ public class LevelScreen implements Screen {
         pauseButtonSprite.draw(spriteBatch);
         // catapultSprite.draw(spriteBatch);
         // grass.draw(spriteBatch);
-        for (Sprite birdSprite : birdSprites) {
-            birdSprite.draw(spriteBatch);
-        }
+        // for (Sprite birdSprite : birdSprites) {
 
+        //     birdSprite.setCenter(birdSprite., delta);
+        //     birdSprite.draw(spriteBatch);
+        // }
 
+        // for(int i = 0; i < birdSprites.size(); i++){
+        //     birdSprites.get(i).setCenter(birdBodies.get(i).getWorldCenter().x, birdBodies.get(i).getWorldCenter().y);
+        // }
+        // for (Body birdBody : birdBodies) {
+        //     birdBody.getUserData().
+        // }
         for (Sprite blockSprite : blockSprites) {
             blockSprite.draw(spriteBatch);
         }
@@ -185,11 +283,29 @@ public class LevelScreen implements Screen {
         for (Sprite pigSprite : pigSprites) {
             pigSprite.draw(spriteBatch);
         }
-        groundLayer.draw(spriteBatch);
+        // groundLayer.draw(spriteBatch);
         spriteBatch.end();
+
+        // debugRenderer.re
+        debugRenderer.render(world, camera.combined);
+		world.step(1/100f, 6, 2);
+        // world.step(1/60f, 6, 2);
+        // debugRenderer.render(world, camera.combined);
 
     }
 
+    // private float accumulator = 0;
+
+    // private void doPhysicsStep(float deltaTime) {
+    //     // fixed time step
+    //     // max frame time to avoid spiral of death (on slow devices)
+    //     float frameTime = Math.min(deltaTime, 0.25f);
+    //     accumulator += frameTime;
+    //     while (accumulator >= Constants.TIME_STEP) {
+    //         WorldManager.world.step(Constants.TIME_STEP, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
+    //         accumulator -= Constants.TIME_STEP;
+    //     }
+    // }
 
 
     @Override
@@ -228,8 +344,8 @@ public class LevelScreen implements Screen {
     public void dispose() {
         // Destroy screen's assets here.
 
-        level.getBuilding().translate(-pigBaseSprite.getX(),-(pigBaseSprite.getY() + pigBaseSprite.getHeight()+10));
-        
+        // level.getBuilding().translate(-pigBaseSprite.getX(),-(pigBaseSprite.getY() + pigBaseSprite.getHeight()+10));
+
     }
 
 
